c257744ef0dcc4babaee1fe7d255009fa0d1f24e
objyg <1729248919@qq.com>
2020-06-16 15:44:36 +0800

    修改了验证码和注册

--- 后端/src/main/java/com/example/demo/controller/EnRollController.java
+++ 后端/src/main/java/com/example/demo/controller/EnRollController.java
@@ -124,31 +124,32 @@ public class EnRollController {
         // 生成图片
         int w = 100, h = 30;
         OutputStream out = response.getOutputStream();
-        VerifyCodeUtils.outputImage(w, h, out, verifyCode);
+         VerifyCodeUtils.outputImage(w, h, out, verifyCode);
 
     }
 
-
 //    注册
     @PostMapping("/enRoll")
-    public String enRollTest(@RequestBody String[] condition){
+    public Result enRollTest(@RequestBody String[] condition){
+        System.out.println("1");
        String inputVerCode=condition[5];
-
-        String types="student";
-
-
+        Result result = new Result();
+        String types=condition[6];
 //    将用户输入的结果与验证码比较
         if(inputVerCode.equals(verifyCode)){
+            System.out.println("2");
             if(types.equals("student")) {
                 int studentId = Integer.parseInt(condition[0]);
                 String name=condition[1];
                 String phone=condition[2];
                 String school=condition[3];
-                String userName="";
+                String userName=condition[7];
                 String passWord=condition[4];
                 Student student = new Student(studentId, name, phone, school, userName, passWord);
                 studentService.insertStudent(student);
-                return "注册成功！";
+                result.setCode(200);
+                result.setMessage("学生注册成功！");
+                return result;
             }
             else {
                 int teacherId=Integer.parseInt(condition[0]);
@@ -159,11 +160,15 @@ public class EnRollController {
                 String passWord=condition[4];
                 Teacher teacher=new Teacher(teacherId,name,email,school,userName,passWord);
                 teacherService.insertTeacher(teacher);
-                return "注册成功！";
+                result.setCode(200);
+                result.setMessage("教师/助教注册成功！");
+                return result;
             }
         }
         else {
-            return "验证码错误！";
+            result.setCode(201);
+            result.setMessage("验证码错误!");
+            return result;
         }
     }
 
